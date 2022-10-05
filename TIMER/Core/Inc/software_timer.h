d96e7df82a36ff0e6b0a9413740c3c138c144ba6
Hong Phat <nguyenhongphat22911@gmail.com>
2022-10-05 14:33:09 +0700

    EX1

---
+++ TIMER/Core/Inc/software_timer.h
@@ -0,0 +1,15 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 5, 2022
+ *      Author: Hong Phat
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+void setTimer(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
